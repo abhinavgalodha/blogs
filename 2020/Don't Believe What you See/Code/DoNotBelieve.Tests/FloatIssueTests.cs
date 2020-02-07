445b02329ed2d71e332f0a3865bbd801a7d4edc1
Abhinav Galodha <abhinav9galodha@gmail.com>
2020-02-07 17:19:53 -0600

    Added code for the Don't Believe Article

---
+++ 2020/Don't Believe What you See/Code/DoNotBelieve.Tests/FloatIssueTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Strings.Test
+{
+    public class FloatIssueTests
+    {
+        [Fact]
+        public void WhenDecimalComparisonInFloat()
+        {
+            Assert.Equal(0.3, 0.1 + 0.1 + 0.1);
+        }
+    }
+}
---
+++ 2020/Don't Believe What you See/Code/DoNotBelieve.Tests/StringTests.cs
@@ -0,0 +1,52 @@
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace Unicode.Test
+{
+    [SuppressMessage("ReSharper", "StringLiteralTypo")]
+    public class StringTests
+    {
+        [Fact]
+        public void WhenStringContains_UnEscapedBackSlash_ThenSplit()
+        {
+            // Arrange
+            var givenString = "Company\vendorid";
+
+            // Act
+            var splittedArray = givenString.Split(@"\");
+
+            // Assert
+            Assert.Equal(2, splittedArray.Length);
+        }
+
+        [Fact]
+        public void WhenStringContains_BackSlash_WithVerbatimString_ThenSplit_Working()
+        {
+            // Arrange
+            var givenString = @"Company\vendorid";
+
+            // Act
+            var splittedArray = givenString.Split(@"\");
+
+            // Assert
+            Assert.Equal(2, splittedArray.Length);
+        }
+
+        [Fact]
+        public void WhenStringContains_BackSlashWithEscapeCharacter_ThenSplit_Working()
+        {
+            // Arrange
+            var givenString = "Company\\vendorid";
+
+            // Act
+            var splittedArray = givenString.Split(@"\");
+
+            // Assert
+            Assert.Equal(2, splittedArray.Length);
+        }
+
+        
+    }
+}
