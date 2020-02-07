d364de6132bf3d22c47a105221bde98883108ca0
Abhinav Galodha <abhinav9galodha@gmail.com>
2020-02-07 17:28:58 -0600

    Added Related problems with Floating Point Arithmetic.

--- 2020/Don't Believe What you See/Code/DoNotBelieve.Tests/FloatIssueTests.cs
+++ 2020/Don't Believe What you See/Code/DoNotBelieve.Tests/FloatIssueTests.cs
@@ -8,9 +8,15 @@ namespace Strings.Test
     public class FloatIssueTests
     {
         [Fact]
-        public void WhenDecimalComparisonInFloat()
+        public void WhenComparisonInFloat()
         {
             Assert.Equal(0.3, 0.1 + 0.1 + 0.1);
+        }
+
+        [Fact]
+        public void WhenComparisonInDecimal()
+        {
+            Assert.Equal(0.3M, 0.1M + 0.1M + 0.1M);
         }
     }
 }
